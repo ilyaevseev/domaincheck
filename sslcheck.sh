330bc4caa7a49ce5890a510bea7d6ae7a49e4443
root <root@s1.(none)>
2016-06-18 01:34:13 +0300

    sslcheck.sh added.

---
+++ sslcheck.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+Alert() {
+	logger -p "user.err" -t "${0##*/}" -s -- "$@"
+	echo "$@" | mail -s "${0##*/} alert on $(hostname -f)" admins
+}
+
+test $# = 1 || { echo "Usage: ${0##*/} hostlist.txt"; exit 1; }
+List="$1"
+
+ Now="$(date +%s)"
+Soon="$(date +%s -d 'next week')"
+
+while read line; do
+	line="${line%#*}" ; test -z "$line" && continue
+	host="${line%:*}"
+	port="${line#*:}" ; test "$port" = "$host" && port=443
+	#echo host = $host, port = $port
+	read d1 d2 <<< $(echo | openssl s_client -connect "$host:$port" 2>/dev/null | openssl x509 -noout -dates | awk -F= '{print "date +%s -d \"",$2,"\""}' | sh -)
+	test "$d1" -gt "$Now"  && Alert "$host:$port is in future." "($d1, $Now)"
+	test "$d2" -lt "$Now"  && Alert "$host:$port expired."      "($d2, $Now)"
+	test "$d2" -lt "$Soon" && Alert "$host:$port expired soon." "($d2, $Soon)"
+done < "$List"
+
+## END ##
